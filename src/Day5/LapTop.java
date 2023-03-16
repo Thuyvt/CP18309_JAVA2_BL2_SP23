bee4d4f6e352cb0e2a7333a08f38721274f25610
thuyvt <thuyvt1005@gmail.com>
2023-03-16 13:53:11 +0700

    day 5

---
+++ src/Day5/LapTop.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day5;
+
+/**
+ *
+ * @author ThuyVT
+ */
+public class LapTop {
+    private String ten;
+    private double gia;
+    private String hang;
+    private int oCung; // 1 - SSD, 2- HDD
+    private boolean trangThai;
+
+    public LapTop() {
+    }
+
+    public LapTop(String ten, double gia, String hang, int oCung, boolean trangThai) {
+        this.ten = ten;
+        this.gia = gia;
+        this.hang = hang;
+        this.oCung = oCung;
+        this.trangThai = trangThai;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public String getHang() {
+        return hang;
+    }
+
+    public void setHang(String hang) {
+        this.hang = hang;
+    }
+
+    public int getoCung() {
+        return oCung;
+    }
+
+    public void setoCung(int oCung) {
+        this.oCung = oCung;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+    
+}
